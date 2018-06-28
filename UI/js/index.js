828d0ff81960bba884649be34219385be33baba0
Femi-DD <femi.highsky@yahoo.com>
2018-06-28 10:50:17 +0100

    Updated UI
     - Added History Page
     - Updated Styles
     - Added sticky footer
     - Changes rides border color
     - Used background color as border in profile
     - Removed "RIde Left" button

--- UI/js/index.js
+++ UI/js/index.js
@@ -1,34 +1,19 @@
 let signinTab = document.getElementById('signin');
 let signupTab = document.getElementById('signup');
-let button_signin = document.getElementById('button_signin');
-let button_signup = document.getElementById('button_signup');
-let plates = document.getElementById('plates');
-let model = document.getElementById('model');
+let button_signin = document.getElementById('signin_tab');
+let button_signup = document.getElementById('signup_tab');
 
-document.getElementById('button_signin').addEventListener('click', function () {
+document.getElementById('signin_tab').addEventListener('click', function () {
    signinTab.style.display = "initial";
    signupTab.style.display = "none";
+   signinTab.removeAttribute('style');
    button_signin.setAttribute('class', 'button active');
    button_signup.setAttribute('class', 'button');
 });
 
-document.getElementById('button_signup').addEventListener('click', function () {
+document.getElementById('signup_tab').addEventListener('click', function () {
    signinTab.style.display = "none";
-   signupTab.style.display = "initial";
+   signupTab.style.display = "block";
    button_signup.setAttribute('class', 'button active');
    button_signin.setAttribute('class', 'button');
 });
-
-function checkAccount(type) {
-   switch (type) {
-      case 'driver':
-         plates.style.display = 'initial';
-         model.style.display = 'initial';
-         break;
-      case 'passenger':
-         plates.style.display = 'none';
-         model.style.display = 'none';
-         break;
-   }
-
-}
