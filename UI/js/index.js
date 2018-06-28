65a12c3e5725ca5c7d441c5682901307824753e8
Femi_DD <femi.highsky@yahoo.com>
2018-06-28 12:25:31 +0100

    Revert "ft-158686860-gh-pages"

--- UI/js/index.js
+++ UI/js/index.js
@@ -1,19 +1,34 @@
 let signinTab = document.getElementById('signin');
 let signupTab = document.getElementById('signup');
-let button_signin = document.getElementById('signin_tab');
-let button_signup = document.getElementById('signup_tab');
+let button_signin = document.getElementById('button_signin');
+let button_signup = document.getElementById('button_signup');
+let plates = document.getElementById('plates');
+let model = document.getElementById('model');
 
-document.getElementById('signin_tab').addEventListener('click', function () {
+document.getElementById('button_signin').addEventListener('click', function () {
    signinTab.style.display = "initial";
    signupTab.style.display = "none";
-   signinTab.removeAttribute('style');
    button_signin.setAttribute('class', 'button active');
    button_signup.setAttribute('class', 'button');
 });
 
-document.getElementById('signup_tab').addEventListener('click', function () {
+document.getElementById('button_signup').addEventListener('click', function () {
    signinTab.style.display = "none";
-   signupTab.style.display = "block";
+   signupTab.style.display = "initial";
    button_signup.setAttribute('class', 'button active');
    button_signin.setAttribute('class', 'button');
 });
+
+function checkAccount(type) {
+   switch (type) {
+      case 'driver':
+         plates.style.display = 'initial';
+         model.style.display = 'initial';
+         break;
+      case 'passenger':
+         plates.style.display = 'none';
+         model.style.display = 'none';
+         break;
+   }
+
+}
